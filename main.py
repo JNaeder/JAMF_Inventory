30d83c538c6dec8f32c558d56f3613a212950da3
= <=>
2023-05-31 19:19:58 -0400

    More refactoring

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pandas as pd
 import gspread as gs
 from gspread_dataframe import set_with_dataframe
 from datetime import datetime, timezone
+from typing import Dict
 
 load_dotenv()
 
@@ -24,40 +25,19 @@ class jamfAPI:
         self.data_frame = pd.DataFrame()
         self.service_account = gs.service_account("./google_credentials.json")
         self.spreadsheet = self.service_account.open_by_key(self.sheet_url)
+        self.amount = 1000
+        self.size = 5
+        self.current_page = 0
 
         self.groups = {
-            "All Computers": {
-                "ws_id": 0,
-                "container": []
-            },
-            "NY Student": {
-                "ws_id": 1745485465,
-                "container": []
-            },
-            "MIA Student": {
-                "ws_id": 1777779477,
-                "container": []
-            },
-            "NAS Student": {
-                "ws_id": 60083025,
-                "container": []
-            },
-            "ATL Student": {
-                "ws_id": 2037246095,
-                "container": []
-            },
-            "CHI Student": {
-                "ws_id": 679638181,
-                "container": []
-            },
-            "General Staff": {
-                "ws_id": 1531308029,
-                "container": []
-            },
-            "No Group": {
-                "ws_id": 904518589,
-                "container": []
-            },
+            "All Computers": {"ws_id": 0, "container": []},
+            "NY Student": {"ws_id": 1745485465, "container": []},
+            "MIA Student": {"ws_id": 1777779477, "container": []},
+            "NAS Student": {"ws_id": 60083025, "container": []},
+            "ATL Student": {"ws_id": 2037246095, "container": []},
+            "CHI Student": {"ws_id": 679638181, "container": []},
+            "General Staff": {"ws_id": 1531308029, "container": []},
+            "No Group": {"ws_id": 904518589, "container": []},
         }
         self.app_names = ["Pro Tools.app", "Ableton Live 10 Standard.app", "Ableton Live 11 Standard.app",
                           "Logic Pro X.app", "8x8 Work.app", "Slack.app", "FortiClient.app", "TeamViewer.app",
@@ -69,26 +49,33 @@ class jamfAPI:
         response = self.session.post(url, auth=(self.username, self.password))
         return response.json()["token"]
 
-    def get_computer_info(self):
-        page = 0
-        size = 5
-        amount = 1000
+    def api_request(self) -> Dict:
+        """
+        Get the data from the JAMF API
+        :return: Dictionary of the http response Dataa
+        """
+        print(".", end=("" if self.current_page % 40 != 0 else "\n"))
+        url = self.base_url + f"/api/v1/computers-inventory?section=GENERAL&section=APPLICATIONS" \
+                              f"&section=GROUP_MEMBERSHIPS&section=HARDWARE&section=OPERATING_SYSTEM" \
+                              f"&section=STORAGE&page={self.current_page}&page-size={self.size}&sort=general.name%3Aasc"
+        response = self.session.get(url)
+        if response.status_code != 200:
+            print(f"Response code was {response.status_code}")
+            return None
+        self.amount = response.json()["totalCount"]
+        return response.json()["results"]
 
-        while (page * size) < amount:
-            # print(f"---Get page {page}---")
-            print(".", end="")
-            url = self.base_url + f"/api/v1/computers-inventory?section=GENERAL&section=APPLICATIONS" \
-                                  f"&section=GROUP_MEMBERSHIPS&section=HARDWARE&section=OPERATING_SYSTEM" \
-                                  f"&section=STORAGE&page={page}&page-size={size}&sort=general.name%3Aasc"
-            response = self.session.get(url)
-            if response.status_code != 200:
-                print(f"Response code was {response.status_code}")
-                page += 1
+    def get_computer_info(self):
+
+        while (self.current_page * self.size) < self.amount:
+            data = self.api_request()
+
+            if data is None:
+                self.current_page += 1
                 continue
-            amount = response.json()["totalCount"]
-            data = response.json()["results"]
-            # print(f"returned {len(data)} items")
+
             for computer in data:
+                # TODO: Do something with this
                 has_group = False
                 new_data = {}
                 general_info = computer["general"]
@@ -97,8 +84,8 @@ class jamfAPI:
                 hardware = computer["hardware"]
                 operatingSystem = computer["operatingSystem"]
                 groups = computer["groupMemberships"]
-                # application_names = [app["name"] for app in applications]
 
+                # TODO: Make this a function
                 new_data["Name"] = general_info["name"]
                 new_data["Specs"] = ""
                 new_data["Serial"] = hardware["serialNumber"]
@@ -115,6 +102,7 @@ class jamfAPI:
                 new_data["CPU"] = hardware["processorType"]
                 new_data["RAM"] = hardware["totalRamMegabytes"] // 1000
 
+                # TODO: Make this another function
                 storage = computer["storage"]["disks"]
                 total_space = 0
                 available_space = 0
@@ -128,12 +116,14 @@ class jamfAPI:
                     new_data["Total Space"] = total_space
                     new_data["Available Space"] = available_space
 
+                # TODO: Make this a function
                 new_data["Apps"] = ""
                 for app in applications:
                     if app["name"] in self.app_names:
                         app_name = app["name"].replace(".app", "")
                         new_data[app_name] = app["version"].split(" ")[0]
 
+                # TODO: This too a function
                 for group in groups:
                     if group["groupName"] in self.groups:
                         group_name = group["groupName"]
@@ -144,14 +134,16 @@ class jamfAPI:
                 if not has_group:
                     self.groups["No Group"]["container"].append(new_data)
                 self.groups["All Computers"]["container"].append(new_data)
-            page += 1
+            self.current_page += 1
 
+        print("\nWriting to Google Sheets")
         for group_name, items in self.groups.items():
             ws_id = items["ws_id"]
             container = items["container"]
             ss = self.spreadsheet.get_worksheet_by_id(ws_id)
             ss.clear()
             set_with_dataframe(ss, pd.DataFrame(container))
+        print("Done!")
 
 
 if __name__ == "__main__":
