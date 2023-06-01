8e6aafedd4b9eb9555fc2bcc526a1a6af4c3a1d4
jnaederSAE <j.naeder@sae.edu>
2023-06-01 00:04:00 -0400

    Small formatting and refactoring

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import pandas as pd
 import gspread as gs
 from gspread_dataframe import set_with_dataframe
 from datetime import datetime, timezone
-from typing import Dict
+from typing import Dict, List
 
 load_dotenv()
 
@@ -20,6 +20,7 @@ class JamfAPI:
     """
     Main JAMF API class
     """
+
     def __init__(self):
         self.base_url = "https://saena.jamfcloud.com/"
         self.username = os.environ.get("JAMF_USERNAME")
@@ -60,28 +61,51 @@ class JamfAPI:
         response = self.session.post(url, auth=(self.username, self.password))
         return response.json()["token"]
 
-    def api_request(self) -> Dict:
+    def api_request(self) -> List[Dict]:
         """
-        Get the data from the JAMF API
+        Get the data from the JAMF API, and return the results
+        :param: None
         :return: Dictionary of the http response Dataa
         """
         print(".", end=("" if self.current_page % 40 != 0 else "\n"))
-        url = self.base_url + f"/api/v1/computers-inventory" \
-                              f"?section=GENERAL" \
-                              f"&section=APPLICATIONS" \
-                              f"&section=GROUP_MEMBERSHIPS" \
-                              f"&section=HARDWARE" \
-                              f"&section=OPERATING_SYSTEM" \
-                              f"&section=STORAGE" \
-                              f"&page={self.current_page}" \
-                              f"&page-size={self.size}" \
-                              f"&sort=general.name%3Aasc"
+        url = self.base_url + ("/api/v1/computers-inventory"
+                               "?section=GENERAL"
+                               "&section=APPLICATIONS"
+                               "&section=GROUP_MEMBERSHIPS"
+                               "&section=HARDWARE"
+                               "&section=OPERATING_SYSTEM"
+                               "&section=STORAGE"
+                               f"&page={self.current_page}"
+                               f"&page-size={self.size}"
+                               "&sort=general.name%3Aasc")
         response = self.session.get(url)
         if response.status_code != 200:
             print(f"Response code was {response.status_code}")
             return None
         self.amount = response.json()["totalCount"]
-        return response.json()["results"]
+        data = response.json()["results"]
+        return data
+
+    @staticmethod
+    def get_hd_data(computer: Dict) -> (int, int):
+        """
+        Parses through the disk info to return relevant disk space.
+        :param computer: Dict of a computer
+        :return: Tuple containing the total space and available space.
+        """
+        storage = computer["storage"]["disks"]
+        total_space = 0
+        available_space = 0
+        if storage:
+            for drive in storage:
+                if drive["device"] == "disk0":
+                    for partition in drive["partitions"]:
+                        if partition["partitionType"] == "BOOT":
+                            total_space += partition[
+                                               "sizeMegabytes"] // 1000
+                            available_space += partition[
+                                                   "availableMegabytes"] // 1000
+            return total_space, available_space
 
     def get_computer_info(self):
 
@@ -109,30 +133,25 @@ class JamfAPI:
                 new_data["Serial"] = hardware["serialNumber"]
                 new_data["Last IP"] = general_info["lastReportedIp"]
                 jamf_binary = general_info["jamfBinaryVersion"]
-                new_data["JAMF Binary"] = jamf_binary.split("-")[0] if jamf_binary else ""
+                new_data["JAMF Binary"] = jamf_binary.split("-")[
+                    0] if jamf_binary else ""
                 last_contact_time = general_info["lastContactTime"]
-                last_contact = datetime.fromisoformat(last_contact_time) if last_contact_time else None
-                days_since_contact = (datetime.now(timezone.utc) - last_contact).days if last_contact_time else 0
-                new_data["Last Contact"] = last_contact.strftime("%m/%d/%Y") if last_contact_time else None
+                last_contact = datetime.fromisoformat(
+                    last_contact_time) if last_contact_time else None
+                days_since_contact = (datetime.now(
+                    timezone.utc) - last_contact).days if last_contact_time \
+                    else 0
+                new_data["Last Contact"] = last_contact.strftime(
+                    "%m/%d/%Y") if last_contact_time else None
                 new_data["Days Since Contact"] = days_since_contact
                 new_data["OS"] = operating_system["version"]
                 new_data["Model"] = hardware["modelIdentifier"]
                 new_data["CPU"] = hardware["processorType"]
                 new_data["RAM"] = hardware["totalRamMegabytes"] // 1000
 
-                # TODO: Make this another function
-                storage = computer["storage"]["disks"]
-                total_space = 0
-                available_space = 0
-                if storage:
-                    for drive in storage:
-                        if drive["device"] == "disk0":
-                            for partition in drive["partitions"]:
-                                if partition["partitionType"] == "BOOT":
-                                    total_space += partition["sizeMegabytes"] // 1000
-                                    available_space += partition["availableMegabytes"] // 1000
-                    new_data["Total Space"] = total_space
-                    new_data["Available Space"] = available_space
+                total_space, available_space = self.get_hd_data(computer)
+                new_data["Total Space"] = total_space
+                new_data["Available Space"] = available_space
 
                 # TODO: Make this a function
                 new_data["Apps"] = ""
@@ -154,6 +173,7 @@ class JamfAPI:
                 self.groups["All Computers"]["container"].append(new_data)
             self.current_page += 1
 
+        # TODO: Make this a separate function too
         print("\nWriting to Google Sheets")
         for group_name, items in self.groups.items():
             ws_id = items["ws_id"]
